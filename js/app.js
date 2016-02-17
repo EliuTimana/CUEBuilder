d7c53fb86ec4d7fa1d2e29f30d732f47dd85179c
Eliu Timana <eliutimana@gmail.com>
2016-02-16 22:30:43 -0500

    performer model view

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
-(function(){
-    var CUEBuilder = angular.module('CUEBuilder',[]);
+(function () {
+    var CUEBuilder = angular.module('CUEBuilder', []);
+    CUEBuilder.controller('EditorController', ['$scope', function ($scope) {
+        $scope.cue = {
+            performer: '',
+            output: ''
+        };
+        $scope.$watchCollection('cue.performer', function () {
+            if ($scope.cue.performer) {
+                $scope.cue.output = 'PERFORMER: "' + $scope.cue.performer + '"';
+            } else {
+                $scope.cue.output = '';
+            }
 
+            //console.log($scope.cue.output);
+        });
+    }]);
 })();
