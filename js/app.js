886244dfc2fca79b3cc50f79725b54142b5dd533
Eliu Timana <eliutimana@gmail.com>
2016-02-17 11:41:37 -0500

    cambios data binding

--- js/app.js
+++ js/app.js
@@ -3,14 +3,20 @@
     CUEBuilder.controller('EditorController', ['$scope', function ($scope) {
         $scope.cue = {
             performer: '',
+            title: '',
+            filename: '',
+            tracklist: '',
             output: ''
         };
-        $scope.$watchCollection('cue.performer', function () {
-            if ($scope.cue.performer) {
-                $scope.cue.output = 'PERFORMER: "' + $scope.cue.performer + '"';
-            } else {
-                $scope.cue.output = '';
-            }
+
+        $scope.$watchCollection('cue', function () {
+            var performer = 'PERFORMER "' + $scope.cue.performer + '"'
+            var title = '\nTITLE "' + $scope.cue.title + '"';
+            var filename = '\nFILE "' + $scope.cue.filename + '" MP3';
+
+            console.log("artista -     cancion nueva(un remix)".split("-")[1].trim());
+
+            $scope.cue.output = performer + title + filename;
 
             //console.log($scope.cue.output);
         });
